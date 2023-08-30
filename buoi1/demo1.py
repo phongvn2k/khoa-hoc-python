e0671520cbdc373fd1af97075363e037b0fb0e42
phongvn2k <vungocphong2k@gmail.com>
2023-08-30 12:16:34 +0700

    update ciode

---
+++ buoi1/demo1.py
@@ -0,0 +1,27 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
+
+# a1 = 1
+# a2 = 2
+# a3 = 3
+# print(a1)
+# print(a2)
+# print(a3)
+
+listDemo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# print(listDemo[5])
+# print(listDemo)
+
+# print(len(listDemo))
+for i in listDemo:
+    if i > 2 or i < 6:
+        print(i)
